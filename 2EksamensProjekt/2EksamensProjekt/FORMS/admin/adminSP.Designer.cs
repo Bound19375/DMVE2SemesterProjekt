871c8a9e9d712fdeccfad161495a98903f87810d
Bound19375 <54005601+Bound19375@users.noreply.github.com>
2022-04-23 14:36:55 +0200

    SP Panels

--- 2EksamensProjekt/2EksamensProjekt/FORMS/admin/adminSP.Designer.cs
+++ 2EksamensProjekt/2EksamensProjekt/FORMS/admin/adminSP.Designer.cs
@@ -28,14 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1119, 685);
+            this.panel1.TabIndex = 0;
             // 
             // adminSP
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1143, 750);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "adminSP";
             this.Text = "adminSP";
             this.ResumeLayout(false);
@@ -43,5 +53,7 @@
         }
 
         #endregion
+
+        private Panel panel1;
     }
 }
--- 2EksamensProjekt/2EksamensProjekt/FORMS/secretary/secretarySP.Designer.cs
+++ 2EksamensProjekt/2EksamensProjekt/FORMS/secretary/secretarySP.Designer.cs
@@ -28,14 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1119, 705);
+            this.panel1.TabIndex = 1;
             // 
             // secretarySP
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1143, 750);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "secretarySP";
             this.Text = "secretarySP";
             this.ResumeLayout(false);
@@ -43,5 +53,7 @@
         }
 
         #endregion
+
+        private Panel panel1;
     }
 }

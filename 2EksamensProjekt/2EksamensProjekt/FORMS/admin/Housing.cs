a52099e439f111c4cef1338d1e0d578bd52a11da
Haurum <67760116+landhaurum@users.noreply.github.com>
2022-05-02 10:55:40 +0200

    Housing Fix

--- 2EksamensProjekt/2EksamensProjekt/FORMS/admin/Housing.cs
+++ 2EksamensProjekt/2EksamensProjekt/FORMS/admin/Housing.cs
@@ -70,7 +70,7 @@ namespace _2EksamensProjekt.FORMS.admin
                         sql = $"SELECT h.id, h.`type`, h.rental_price, h.m2 FROM housing h WHERE h.id NOT IN(SELECT hr2.housing_id FROM housing_residents hr2) AND h.rental_price BETWEEN @min AND @max GROUP BY h.id ORDER BY h.id;";
                     }
                     dal.Gridview(dataGridView1, "SELECT a.username, w.type FROM waitlist w, account a WHERE w.account_username = a.username ORDER BY a.username;", false);
-                    dal.Gridview(dataGridView2, sql, false);
+                    dal.Gridview(dataGridView2, sql, true);
                     dal.Gridview(dataGridView3, "SELECT hr.housing_id, h.`type`, h.m2, h.rental_price, r.name, hr.start_contract, hr.residents_username  FROM housing h, housing_residents hr, residents r WHERE h.id = hr.housing_id and hr.residents_username = r.account_username;", false);
 
 

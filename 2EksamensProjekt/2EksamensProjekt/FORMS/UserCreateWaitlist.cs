1159a2827ca447695c6b51bad8b4faffd8ef0549
Haurum <67760116+landhaurum@users.noreply.github.com>
2022-05-02 21:55:52 +0200

    Use Global Usings

--- 2EksamensProjekt/2EksamensProjekt/FORMS/UserCreateWaitlist.cs
+++ 2EksamensProjekt/2EksamensProjekt/FORMS/UserCreateWaitlist.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using DAL;
-
-namespace _2EksamensProjekt
+﻿namespace _2EksamensProjekt
 {
     public partial class UserCreateWaitlist : Form
     {        

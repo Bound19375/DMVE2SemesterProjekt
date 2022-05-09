c7d6b51345ad9e6f933886372d18c6d36ff72ab6
Haurum <67760116+landhaurum@users.noreply.github.com>
2022-05-09 10:55:29 +0200

    sma

--- 2EksamensProjekt/2EksamensProjekt/DAL.cs
+++ 2EksamensProjekt/2EksamensProjekt/DAL.cs
@@ -88,7 +88,7 @@ public class API
                     return 
                         "SELECT a.username AS 'Brugernavn', a.`type`AS 'Type' " +
                         "FROM account a " +
-                        "WHERE a.privilege = 'waitlist' ";
+                        "WHERE a.privilege = 'waitlist';";
                 
                 case SELECTSQLQUERY.CurrentResidents: 
                     return 
